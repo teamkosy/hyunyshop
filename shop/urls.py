e96809ea28b5eb35ba75fc37f62b2532fb8c2a80
teamkosy <shimwh81@naver.com>
2021-03-31 14:22:00 +0900

    0331

--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-    # path('', views.index, name='product_all'),
     path('', views.product_in_category, name='product_all'),
     path('<slug:category_slug>/', views.product_in_category, name='product_in_category'),
     path('<int:id>/<product_slug>/', views.product_detail, name='product_detail'),
